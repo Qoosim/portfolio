6bf2421565c65ffcde79a650fefe91b631b56683
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-05-24 19:03:48 +0100

    Add languages for each of the projects list

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -7,7 +7,7 @@ const Project = () => {
   const [readMore, setReadMore] = React.useState(false);
 
   return (
-    <Container className="mt-5">
+    <Container className="mt-5 pb-5">
       <Row xs={1} md={2} lg={3} className="g-4">
         {projects.map((project) => {
           const { id, name, github, live, image, tech, desc } = project;
@@ -21,12 +21,20 @@ const Project = () => {
                     {readMore ? desc : desc.slice(0, 100)}
                     <button>{readMore ? " Read Less" : " Read More"}</button>
                   </Card.Text>
+                  { tech.map((language, index) => {
+                      console.log(language, index);
+                      return (
+                        <ul className="d-flex justify-content-center">
+                            <li key={index}>{language}</li>
+                        </ul>
+                      )
+                  })}
                   <Button variant="primary" className="bg-dark">
-                    <a href={live} className="text-white">
+                    <a href={github} className="text-white">
                       <FaGithub />
                     </a>
                   </Button>
-                  <Button variant="secondary">Live</Button>
+                  <Button variant="secondary"><a href={live}>Live</a></Button>
                 </Card.Body>
               </Card>
             </Col>

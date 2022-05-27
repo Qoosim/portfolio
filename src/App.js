10b0608b22fd38297bdcac076f403638be62181d
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-05-27 18:45:50 +0100

    Modify App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {                                                                                                    BrowserRouter as Router,
+import {
+  BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';

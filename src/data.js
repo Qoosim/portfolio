3e45328111c9143e379d88680dddf9c54e414798
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-05-27 18:41:21 +0100

    Remove unused variable

--- src/data.js
+++ src/data.js
@@ -1,10 +1,4 @@
 import { v4 as uuidv4 } from 'uuid';
-import {
-  SiJavascript, SiCss3,
-  SiRedux,
-  SiWebpack,
-} from 'react-icons/si';
-import { FaHtml5, FaReact, FaBootstrap } from 'react-icons/fa';
 
 export const projects = [
   {
@@ -13,7 +7,6 @@ export const projects = [
     github: 'https://github.com/Qoosim/JavaScript-Group-Capstone',
     live: 'https://laughing-hodgkin-321112.netlify.app/',
     image: 'https://github.com/Qoosim/JavaScript-Group-Capstone/blob/dev/src/assets/food_menu.png?raw=true',
-    // tech: [<SiJavascript />, <SiWebpack />, <FaHtml />, <SiCss3 />]
     tech: ['JavaScript', 'Webpack', 'Html', 'Css'],
     desc: 'This is a JavaScript capstone project. It is a web application based on an external API. A selected API provides data about different recipes. The webapp has two interfaces. 1. A home page showing a list of items that you can "like." 2. A popup window with more data about an item that you can use to comment on the item.',
   },
@@ -23,7 +16,6 @@ export const projects = [
     github: 'https://github.com/Qoosim/JavaScript-Group-Capstone',
     live: 'https://laughing-hodgkin-321112.netlify.app/',
     image: 'https://github.com/Qoosim/JavaScript-Group-Capstone/blob/dev/src/assets/food_menu.png?raw=true',
-    // tech: [<SiJavascript />, <SiWebpack />, <FaHtml />, <SiCss3 />]
     tech: ['JavaScript', 'Webpack', 'Html', 'Css'],
     desc: 'This is a JavaScript capstone project. It is a web application based on an external API. A selected API provides data about different recipes. The webapp has two interfaces. 1. A home page showing a list of items that you can "like." 2. A popup window with more data about an item that you can use to comment on the item.',
   },
@@ -33,7 +25,6 @@ export const projects = [
     github: 'https://github.com/Qoosim/leaderboard-JS',
     live: 'https://leaderboard-js.netlify.app/',
     image: 'https://github.com/Qoosim/leaderboard-JS/blob/develop/src/images/leader_2.png?raw=true',
-    //tech: [<SiJavascript />, <SiWebpack />, <FaHtml5 />, <SiCss3 />],
     tech: ['JavaScript', 'Webpack', 'Html', 'Css'],
     desc: 'This leaderboard website displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API service.'
   },
@@ -43,7 +34,6 @@ export const projects = [
     github: 'https://github.com/Qoosim/Awesome-Books',
     live: 'https://qoosim-awesome-books.netlify.app/',
     image: 'https://github.com/Qoosim/Awesome-Books/blob/main/images/awesome_bk.png?raw=true',
-    //tech: [<SiJavascript />,  <FaHtml5 />, <SiCss3 />],
     tech: ['JavaScript', 'Html', 'Css'],
     desc: 'This is a project that allows user to add/remove books from both localStorage and browser memory',
   },
@@ -62,7 +52,6 @@ export const projects = [
     github: 'https://github.com/Qoosim/math-magicians-react',
     live: 'https://qoosim-math-magician-react.netlify.app/',
     image: 'https://github.com/Qoosim/math-magicians-react/blob/develop/src/images/calculate.png?raw=true',
-    //tech: [<FaReact />, <SiJavascript />, <FaBootstrap />],
     tech: ['React', 'JavaScript', 'Bootstrap'],
     desc: 'Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: - make simple calculations and read a random math-related quote.',
   },
@@ -72,7 +61,6 @@ export const projects = [
     github: 'https://github.com/JEFFKAY16/space-travelers-hub-react',
     live: 'https://clever-mclean-8316d1.netlify.app/',
     image: 'https://github.com/JEFFKAY16/space-travelers-hub-react/blob/dev/Images/space_tr.png?raw=true',
-    //tech: [<FaReact />, <SiRedux />, <SiJavascript />, <FaBootstrap />],
     tech: ['React', 'Redux', 'JavaScript', 'Bootstrap'],
     desc: 'This is a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.',
   },
@@ -82,7 +70,6 @@ export const projects = [
     github: 'https://github.com/Qoosim/meal-reviews-refactor-react',
     live: 'https://qoosim-meal-reviews-refactor.netlify.app/',
     image: 'https://github.com/Qoosim/meal-reviews-refactor-react/blob/main/src/images/meal_r.png?raw=true',
-    //tech: [<FaReact />, <SiJavascript />, <FaBootstrap />],
     tech: ['React', 'JavaScript', 'Bootstrap'],
     desc: 'This is an application where a user can review all the meal menu by clicking either of the forward or backward arrow. The user can also randomly review the meal menu by clicking on the select random button.',
   }
